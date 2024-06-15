7bc53840b73a0f912d39393f80979d9774f4a506
Igor Pelczarski <igorpe144@gmail.com>
2024-06-15 14:48:30 +0200

    delated unused imports

--- src/App.js
+++ src/App.js
@@ -2,47 +2,47 @@ import { Autor } from "./features/autor/AuthorPage"
 import { TaskPage } from "./features/tasks/TaskPage";
 import { TasksPage } from "./features/tasks/TasksPage/index";
 
-import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import { Container, NavWrapper, StyledNavLink } from "./myGlobalStyle";
 
 export const App = () => (
 
   <Container>
-  <HashRouter>
-    <nav>
-      <NavWrapper>
-        <li>
-          <StyledNavLink activeClassName="active"
-            to="/zadania"
-            style={{ textDecoration: 'none' }}
-          >
-            Zadania
-          </StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink activeClassName="active"
-            to="/author"
-            style={{ textDecoration: 'none'}}
-          >
-            Autor
-          </StyledNavLink>
-        </li>
-      </NavWrapper>
-      <Switch>
-        <Route path="/zadania/:id">
-          <TaskPage />
-        </Route>
-        <Route path="/zadania">
-          <TasksPage />
-        </Route>
-        <Route path="/author">
-          <Autor />
-        </Route>
-        <Route path="/">
-        <Redirect to="/zadania"/>
-        </Route>
-      </Switch>
-    </nav>
-  </HashRouter>
+    <HashRouter>
+      <nav>
+        <NavWrapper>
+          <li>
+            <StyledNavLink activeClassName="active"
+              to="/zadania"
+              style={{ textDecoration: 'none' }}
+            >
+              Zadania
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink activeClassName="active"
+              to="/author"
+              style={{ textDecoration: 'none' }}
+            >
+              Autor
+            </StyledNavLink>
+          </li>
+        </NavWrapper>
+        <Switch>
+          <Route path="/zadania/:id">
+            <TaskPage />
+          </Route>
+          <Route path="/zadania">
+            <TasksPage />
+          </Route>
+          <Route path="/author">
+            <Autor />
+          </Route>
+          <Route path="/">
+            <Redirect to="/zadania" />
+          </Route>
+        </Switch>
+      </nav>
+    </HashRouter>
   </Container>
 )
